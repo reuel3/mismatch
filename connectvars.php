d63860a29044322346632b8cfaafaffec1573648
Skip Kleckner <reuel3@gmail.com>
2012-12-02 12:19:42 -0500

    changes to connect to heroku/xeround database

--- connectvars.php
+++ connectvars.php
@@ -1,9 +1,14 @@
 <?php
   // Define database connection constants
-  define('DB_HOST', 'localhost');
+  /*define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASSWORD', 'skip1234');
-  define('DB_NAME', 'mmudb');
+  define('DB_NAME', 'mmudb');*/
+
+define('DB_HOST', 'int.instance33333.db.xeround.com:14218');
+define('DB_USER', 'app9693544');
+define('DB_PASSWORD', 'skip1234');
+define('DB_NAME', 'mmudb');
 
   // Connect to the database 
   $dbc = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD); 
